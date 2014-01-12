5b9921d85f68401cfcf3e52706a3158f9b07f299
alojzije <shljunak@hotmail.com>
2014-01-12 14:06:10 +0100

    Add MultiplayerActivity nd TicTavToe classes instead of the one MyActivity class.

---
+++ src/com/example/TicTacToe/MultiPlayerActivity.java
@@ -0,0 +1,81 @@
+package com.example.TicTacToe;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MultiPlayerActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_play);
+
+        TicTacToe.clearButtons();
+        TicTacToe.addButton(this,R.id.button1);
+        TicTacToe.addButton(this,R.id.button2);
+        TicTacToe.addButton(this,R.id.button3);
+        TicTacToe.addButton(this,R.id.button4);
+        TicTacToe.addButton(this,R.id.button5);
+        TicTacToe.addButton(this,R.id.button6);
+        TicTacToe.addButton(this,R.id.button7);
+        TicTacToe.addButton(this,R.id.button8);
+        TicTacToe.addButton(this,R.id.button9);
+        TicTacToe.setNameforPlayerX("X");
+        TicTacToe.setNameforPlayerO("O");
+        TicTacToe.setTurn(0);
+
+
+    }
+
+    public void newTurn(View view) {
+        // mark button either for playerO or for playerX
+        if (TicTacToe.turn % 2 == 0)
+            TicTacToe.playO(this, (Button) view);
+        else
+            TicTacToe.playX(this, (Button) view);
+
+        //check if player_o won the game or if it's a DRAW;
+        if (TicTacToe.isGameOver())
+            endGame(TicTacToe.winner);
+
+    }
+
+
+
+    private void endGame(String winner) {
+        Toast toast;
+        //print the winning message
+        if (winner == TicTacToe.DRAW)
+            toast = Toast.makeText(getApplicationContext(), " Draw \n ", Toast.LENGTH_LONG);
+        else
+            toast = Toast.makeText(getApplicationContext(), winner + " won \n Up for a rematch? ", Toast.LENGTH_LONG);
+
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.show();
+        //disable all the buttons
+        TicTacToe.disableAllButtons();
+
+
+    }
+
+
+    public void newGame(View view) {
+
+        if (Build.VERSION.SDK_INT >= 11) {
+            recreate();
+        } else {
+            Intent intent = getIntent();
+            finish();
+            startActivity(intent);
+        }
+    }
+}
--- src/com/example/TicTacToe/MyActivity.java
+++ src/com/example/TicTacToe/MyActivity.java
@@ -24,131 +24,16 @@ public class MyActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main_chooser);
         buttons = new ArrayList<View>();
-        buttons.add(findViewById(R.id.button1));
-        buttons.add(findViewById(R.id.button2));
-        buttons.add(findViewById(R.id.button3));
-        buttons.add(findViewById(R.id.button4));
-        buttons.add(findViewById(R.id.button5));
-        buttons.add(findViewById(R.id.button6));
-        buttons.add(findViewById(R.id.button7));
-        buttons.add(findViewById(R.id.button8));
-        buttons.add(findViewById(R.id.button9));
-        
-        
 
     }
 
-    public void newTurn(View view) {
-
-        turn++;
-        currentBtn = ((Button) view);
-        currentBtn.setEnabled(false);
-
-
-        if (turn%2 != 0){
-            backgroundPic = getResources().getDrawable(R.drawable.purple);
-            currentBtn.setTag("purple");
-            winner = "purple";
-        }else {
-            backgroundPic = getResources().getDrawable(R.drawable.yellow);
-            currentBtn.setTag("yellow");
-            winner = "yellow";
-        }
-
-        //set background pic
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN)
-            currentBtn.setBackgroundDrawable(backgroundPic);
-        else
-            currentBtn.setBackground(backgroundPic);
-
-        //check if the current player just won the game
-        checkIfWon();
-
-
+    public void invokeSinglePlayer(View view) {
     }
 
-    private void checkIfWon() {
-        //the logic
-
-        //check horizontal
-        if (buttons.get(0).getTag()!= null &&
-            buttons.get(0).getTag() == buttons.get(1).getTag() &&
-            buttons.get(1).getTag() == buttons.get(2).getTag()) {
-
-            endGame(winner + " (horizontal 123)");
-        }
-        else if (buttons.get(3).getTag()!= null &&
-                buttons.get(3).getTag() == buttons.get(4).getTag() &&
-                buttons.get(4).getTag() == buttons.get(5).getTag()) {
-
-            endGame(winner + " (horizontal 456)");
-        }
-        else if (buttons.get(6).getTag()!= null &&
-                buttons.get(6).getTag() == buttons.get(7).getTag() &&
-                buttons.get(7).getTag() == buttons.get(8).getTag()) {
-
-            endGame(winner + " (horizontal 789)");
-        }
-
-        //check vertical
-        else if (buttons.get(0).getTag()!= null &&
-                buttons.get(0).getTag() == buttons.get(3).getTag() &&
-                buttons.get(3).getTag() == buttons.get(6).getTag()) {
-
-            endGame(winner + " (vertical 147)");
-        }
-
-        else if (buttons.get(1).getTag()!= null &&
-                buttons.get(1).getTag() == buttons.get(4).getTag() &&
-                buttons.get(4).getTag() == buttons.get(7).getTag()) {
-
-            endGame(winner + " (vertical 258)");
-        }
-
-        else if (buttons.get(2).getTag()!= null &&
-                buttons.get(2).getTag() == buttons.get(5).getTag() &&
-                buttons.get(5).getTag() == buttons.get(8).getTag()) {
-
-            endGame(winner + " (vertical 369)");
-        }
-
-        //check diagonal
-        else if (buttons.get(0).getTag()!= null &&
-                buttons.get(0).getTag() == buttons.get(4).getTag() &&
-                buttons.get(4).getTag() == buttons.get(8).getTag()) {
-
-            endGame(winner + " (diagonal 159)");
-        }
-
-        else if (buttons.get(2).getTag()!= null &&
-                buttons.get(2).getTag() == buttons.get(4).getTag() &&
-                buttons.get(4).getTag() == buttons.get(6).getTag()) {
-
-            endGame(winner + " (diagonal 357)");
-        }
-        else if (turn == 9)
-            endGame("Draw. Nobody");
-    }
-
-    private void endGame(String str) {
-        //print the winning message
-        Toast.makeText(getApplicationContext(), str + " wins", Toast.LENGTH_LONG).show();
-        //disable all the buttons
-        for(View btn: buttons)
-            btn.setEnabled(false);
-
-    }
-
-
-    public void newGame(View view) {
-        if (Build.VERSION.SDK_INT >= 11) {
-            recreate();
-        } else {
-            Intent intent = getIntent();
-            finish();
-            startActivity(intent);
-        }
+    public void invokeMultiPlayer(View view) {
+        Intent intent = new Intent(this, MultiPlayerActivity.class);
+        startActivity(intent);
     }
 }
