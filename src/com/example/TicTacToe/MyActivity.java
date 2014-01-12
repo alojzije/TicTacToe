15fd5558acfe5bd77303738cf96b2257075fca81
alojzije <shljunak@hotmail.com>
2014-01-12 15:43:37 +0100

    Make MyActivity the first screen with user options to choose between single- or multi-play.

--- src/com/example/TicTacToe/MyActivity.java
+++ src/com/example/TicTacToe/MyActivity.java
@@ -2,38 +2,25 @@ package com.example.TicTacToe;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import java.util.ArrayList;
 
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
-    int turn = 0;
-    ArrayList<View> buttons;
-    String winner;
-    Button currentBtn ;
-    Drawable backgroundPic;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_chooser);
-        buttons = new ArrayList<View>();
-
     }
 
     public void invokeSinglePlayer(View view) {
     }
 
     public void invokeMultiPlayer(View view) {
-        Intent intent = new Intent(this, MultiPlayerActivity.class);
+        Intent intent = new Intent("com.example.TicTacToe.MultiPlayerActivity");
         startActivity(intent);
     }
 }
